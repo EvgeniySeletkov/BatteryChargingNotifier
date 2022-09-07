edee2c16897884588083dcfa369b3af413a1529e
Evgeniy <yevhen.selietkov@headworks.com.ua>
2022-09-07 12:13:36 +0300

    - Created project. - Installed Prism.

---
+++ src/BatteryChargingNotifier/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+
+namespace BatteryChargingNotifier
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null)
+            : base(initializer)
+        {
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Navigation.
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync($"/{nameof(NavigationPage)}/{nameof(MainPage)}");
+        }
+    }
+}
